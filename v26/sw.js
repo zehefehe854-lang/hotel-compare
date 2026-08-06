75eecbdfb9f047899f90b44e262a2f5197266b49
Deploy <deploy@hc.local>
2026-08-07 00:47:38 +0800

    Deploy v27: fix CDN cache with no-store SW + version check

--- v26/sw.js
+++
@@ -1,3 +0,0 @@
-self.addEventListener('install', () => self.skipWaiting());
-self.addEventListener('activate', () => self.clients.claim());
-self.addEventListener('fetch', e => e.respondWith(fetch(e.request)));
